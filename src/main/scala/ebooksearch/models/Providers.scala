6a7d3c7739f5326c626714420efd09b7d50a93aa
koduki <pascalm3@gmail.com>
2012-03-20 17:46:56 +0900

    modify index view.

---
+++ src/main/scala/ebooksearch/models/Providers.scala
@@ -0,0 +1,7 @@
+package cn.orz.pascal.scala.ebooksearch.models
+
+object Providers {
+	def eBookJapan = Provider("eBookJapan", "http://www.ebookjapan.jp/")
+	def paburi = Provider("Paburi", "http://www.paburi.com/paburi/")
+	def bookWalker = Provider("BOOK☆WALKER", "http://bookwalker.jp/")
+}
