661717096cdbe44bbc741d93b8f4cfd201f4f9fc
koduki <pascalm3@gmail.com>
2012-05-06 06:51:16 +0900

    fix ConfigReader.

--- src/main/scala/commons/utils/ConfigReader.scala
+++ src/main/scala/commons/utils/ConfigReader.scala
@@ -3,11 +3,19 @@ package cn.orz.pascal.scala.commons.utils
 import com.twitter.util.Eval
 
 object ConfigReader {
-  def apply[T](name: String) = {
-    val path = Thread.currentThread().getContextClassLoader().getResource(name).getPath
-    val file = new java.io.FileInputStream(path)
-    try Eval[T](file)
-    finally file.close
+  val pool = scala.collection.mutable.Map[String, Any]()
+
+  def apply[T](name: String):T = {
+    if (pool.contains(name)) {
+      pool(name).asInstanceOf[T]
+    } else {
+      val path = Thread.currentThread().getContextClassLoader().getResource(name).getPath
+      val file = new java.io.FileInputStream(path)
+      val config = try Eval[T](file)
+      finally file.close
+      pool(name) = config
+      config.asInstanceOf[T]
+    }
   }
 }
 
