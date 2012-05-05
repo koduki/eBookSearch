9de58a9aa1ccd99dbe26d4ae6cd8ac3a28f8d181
koduki <pascalm3@gmail.com>
2012-05-06 00:27:40 +0900

    add LevenshteinDistance library

---
+++ src/main/scala/commons/utils/LevenshteinDistance.scala
@@ -0,0 +1,55 @@
+// vim: set ts=4 sw=4 et:
+package cn.orz.pascal.scala.commons.utils
+
+object LevenshteinDistance {
+  def apply(str1:String, str2:String):Int = {
+    // Make the edit graph array.
+    val colsSize = str1.length
+    val rowsSize = str2.length
+    val distance = new Array[Array[Int]](rowsSize + 1).map(ys => new Array[Int](colsSize + 1))
+    
+    // Initialize the leftmost column.
+    for (val row <- 0 to rowsSize) {
+      distance(row)(0) = row
+    }
+    // Initialize the top row.
+    for (val col <- 0 to colsSize) {
+      distance(0)(col) = col
+    }
+
+    // Fill in the rest of the array.
+    val chars1 = str1.toCharArray()
+    val chars2 = str2.toCharArray()
+    for (val col <- 1 to colsSize) {
+      for (val row <- 1 to rowsSize) {
+        // Fill in entry [r, c].
+        // Check the three possible paths to here.
+        val rightCost = distance(row - 1)(col) + 1 // right value +1
+        val downCost  = distance(row)(col - 1) + 1 // down value +1
+        val diagonalCost = if (chars1(col - 1) == chars2(row - 1)) {
+          // There is a diagonal link.
+          distance(row - 1)(col - 1);
+        } else {
+          Int.MaxValue
+        }
+        
+        distance(row)(col) = min(rightCost, downCost, diagonalCost); 
+      }
+    }
+
+    distance(rowsSize)(colsSize);
+  }
+
+  private def min(rightCost:Int, downCost:Int, diagonalCost:Int) = {
+    if ((rightCost <= downCost) && (rightCost <= diagonalCost)) {
+      // Come from above.
+      rightCost;
+    } else if (downCost <= diagonalCost) {
+      // Come from the left.
+      downCost;
+    } else {
+      // Come from the diagonal.
+      diagonalCost;
+    }
+  }
+}
---
+++ src/test/scala/commons/utils/LevenshteinDistanceTest.scala
@@ -0,0 +1,34 @@
+// vim: set ts=4 sw=4 et:
+package cn.orz.pascal.scala.commons.utils
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class LevenshteinDistanceTest extends WordSpec with ShouldMatchers {
+ 
+  "這いよれ！　ニャル子さん4" when {
+    val str1 = "這いよれ！　ニャル子さん4"
+    "這いよれ！ニャル子さん 9 (GA文庫)" should {
+      "return 11" in {
+      LevenshteinDistance(str1, "這いよれ！ニャル子さん 9 (GA文庫)") should be(11)
+      }
+    }
+    "這いよれ！ニャル子さん 4 (GA文庫)" should {
+      "return 9" in {
+      LevenshteinDistance(str1, "這いよれ！ニャル子さん 4 (GA文庫)") should be(9)
+      }
+    }
+    "這いよれ！ニャル子さん 3 (GA文庫)" should {
+      "return 11" in {
+      LevenshteinDistance(str1, "這いよれ！ニャル子さん 3 (GA文庫)") should be(11)
+      }
+    }
+    "這いよれ!ニャル子さん4 スペシャルボックス(DVD付き)" should {
+      "return 20" in {
+      LevenshteinDistance(str1, "這いよれ!ニャル子さん4 スペシャルボックス(DVD付き)") should be(20)
+      }
+    }
+
+
+
+  }
+}
